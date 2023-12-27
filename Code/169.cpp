602be1f1468522cdbaee0b347af1e65a739d6595
MethuParoi <paroimethu@gmail.com>
2023-12-27 10:25:29 +0530

    solved-1_26-dec-2023

---
+++ Code/169.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        return nums[n/2];
+    }
+};
