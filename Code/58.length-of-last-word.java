99717776423769cd6e31e8c6a82bb10b5551a20d
muhit-khan <muhitkhan_bd@outlook.com>
2023-12-31 15:00:15 +0530

    Refactor lengthOfLastWord

--- Code/58.length-of-last-word.java
+++ Code/58.length-of-last-word.java
@@ -2,15 +2,36 @@
  * [58] Length of Last Word
  */
 
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String[] words = s.split(" ");
+
+//         if (words.length > 0) {
+//             String lastWord = words[words.length - 1];
+//             return lastWord.length();
+//         } else {
+//             return 0;
+//         }
+//     }
+// }
+
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.split(" ");
+        int count = 0;
 
-        if (words.length > 0) {
-            String lastWord = words[words.length - 1];
-            return lastWord.length();
-        } else {
-            return 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
         }
+        for (; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else if (s.charAt(i) == ' ') {
+                break;
+            }
+        }
+        return count;
     }
 }
